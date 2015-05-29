e8ba15e7926fbbfb6540c6555e79eecc51309aa2
dsrikant <dushyant.srikant@gmail.com>
2015-05-29 08:43:37 -0700

    Modified verificcation process

---
+++ Verify.py
@@ -0,0 +1,73 @@
+# Run this on dcswitch
+
+# Check for runs of zeros
+# Open a file?
+import filecmp
+import time
+import os
+import sys
+import hashlib
+
+# Find the shasum of the file
+def hashThis(filename):
+  func = hashlib.md5()
+  f = open(filename, 'rb')
+  try:
+    func.update(f.read())
+  finally:
+    f.close()
+  return func.digest()
+
+
+# Create a new file with the hash of current file
+def hashFile(currFile, hashValue):
+  name = currFile + '_hashed_verification.txt'
+
+  try:
+    #print(name)
+    file = open(name,'w+')
+    file.write(hashValue)
+    file.close()
+    return name
+
+  except:
+    print('Something is up here')
+
+
+
+# Still need a clever way to figure out which files are on the server running
+# the instance of this script
+def verify():
+  while True:                                                                      
+      path = "/mnt/disk1"
+      filelist = sorted(os.listdir(path))
+      for currFile in sorted(filelist):
+        if "hashed" not in str(currFile) and "verified" not in str(currFile) and str(currFile).endswith('.txt') and str(currFile) not in open('verified.txt').read():
+          sp = currFile.split(".")
+          print currFile
+          hash_verify = hashThis(currFile)
+          hash_file_verify = hashFile(sp[0], hash_verify)
+          while True: # Reconsider this while true
+            if not os.path.isfile(sp[0] + "_hashed.txt"):
+              time.sleep(3)
+            else:
+              checksum = filecmp.cmp(sp[0] + "_hashed.txt", hash_file_verify)
+              # Hanging if checksum not verified?
+              if checksum:
+                f = open("verified.txt", "a")
+                f.write(currFile + "\n")
+                f.close()
+                remove1 = "rm " + sp[0] 
+                os.system(remove1)
+              else:
+                remove1 = "rm " + currFile 
+                remove2 = "rm " + sp[0] 
+                remove3 = "rm " + sp[0] + "_hashed.txt" 
+                remove4 = "rm " + sp[0] + "_hashed_verification.txt" 
+                os.system(remove1)
+                os.system(remove2)
+                os.system(remove3)
+                os.system(remove4)
+            break
+
+verify()

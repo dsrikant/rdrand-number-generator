c6d621104c41b1387a06960e30c71e853c33298b
Dushyant Srikant <dushyant.srikant@gmail.com>
2015-06-02 01:17:42 -0700

    Analysis driver, this will organize the analysis process on each disk

---
+++ An_Driver.py
@@ -0,0 +1,16 @@
+import os
+
+#def quicksort():
+    # Do a quicksort on the output of the file
+
+#def move():
+    # Move the file with the sorted data to the scratch server
+
+#def divvy():
+    # Split up the work amongst five servers for starters
+    # Maybe get more clever and split up the work between all the servers that sent files
+
+
+
+cmd = "./a.out 8_kikibytes.txt"
+os.system(cmd)
